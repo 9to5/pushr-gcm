f73ba767fead09dc9e05604a83a2a2538c4e9a0b
Tom Pesman <tom@tnux.net>
2014-06-16 18:02:24 +0200

    added settings object to config spec

--- spec/lib/pushr/configuration_gcm_spec.rb
+++ spec/lib/pushr/configuration_gcm_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'pushr/daemon'
 require 'pushr/configuration_gcm'
 
 describe Pushr::ConfigurationGcm do
@@ -7,7 +8,9 @@ describe Pushr::ConfigurationGcm do
     Pushr::Core.configure do |config|
       config.redis = ConnectionPool.new(size: 1, timeout: 1) { MockRedis.new }
     end
+    Pushr::Daemon.config = settings
   end
+  let(:settings) { Pushr::Daemon::Settings.new }
 
   describe 'all' do
     it 'returns all configurations' do
